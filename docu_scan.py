f13538951757df5aaebbf0a088c499c4810354f0
msarmadsaeed <56521807+msarmadsaeed@users.noreply.github.com>
2023-03-09 23:35:49 -0800

    docu complete

--- docu_scan.py
+++ docu_scan.py
@@ -17,6 +17,7 @@ if not cap.isOpened():
 def getContours(img):
     contours, hei = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     maxArea = 0
+    # biggest = np.zeros((4,1,2))
     biggest = np.array([])
     for cont in contours:
         area = cv2.contourArea(cont)
@@ -24,10 +25,10 @@ def getContours(img):
             # cv2.drawContours(imgCont,cont, -1, (255,0,0),3)
             peri = cv2.arcLength(cont, True)
             apx = cv2.approxPolyDP(cont, peri*0.02, True,)
-            if area>maxArea and len(apx) == 4:
+            if area > maxArea and len(apx) == 4:
                 biggest = apx
                 maxArea = area
-        cv2.drawContours(imgCont,biggest, -1, (255,0,0),3)
+        cv2.drawContours(imgCont,biggest, -1, (255,0,0),5)
     return biggest
 
 def preProcess(img):
@@ -44,13 +45,24 @@ def reorder(mypoints):
     mypointsnew = np.zeros((4,1,2), np.int32)
     add = mypoints.sum(1)
 
+    mypointsnew[0] = mypoints[np.argmin(add)]
+    mypointsnew[3] = mypoints[np.argmax(add)]
+
+    diff = np.diff(mypoints,axis=1)
+
+    mypointsnew[1] = mypoints[np.argmin(diff)]
+    mypointsnew[2] = mypoints[np.argmax(diff)]
+
     return mypointsnew
 
 def getWarp(img, biggest):
-    re = reorder(biggest)
-    pts1 = np.float32(biggest)
-    pts2 = np.float32([[0,0],[wid,0][0,hei],[wid,hei]])
-
+    print(biggest)
+    reord = reorder(biggest)
+    pts1 = np.float32(reord)
+    pts2 = np.float32([[0,0],[wid,0],[0,hei],[wid,hei]])
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgout = cv2.warpPerspective(img,matrix,(wid,hei))
+    return imgout
 #####################################################
 while True:
     ret,img = cap.read()
@@ -58,14 +70,16 @@ while True:
     imgCont = img.copy()
     imgThres = preProcess(img)
     persp = getContours(imgThres)
-    imgWarped = getWarp(img,persp)
+    print(persp)
+    if persp.size > 0:
+        imgWarped = getWarp(img,persp)
 
+        if not ret:
+            print("Could not read frame")
+            break
+        cv2.imshow("video", imgWarped)
 
-    if not ret:
-        print("Could not read frame")
-        break
-
-    cv2.imshow("video", imgWarped)
+    cv2.imshow("vid", imgCont)
    
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
